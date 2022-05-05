fb325cb3c4aa342950d6376c8be5a1f35498d9e0
Miles Owens <milesrhowens@gmail.com>
2022-05-05 12:47:12 -0700

    I figured out how python works so this is the actual start to the yuumi object

--- yuumi.py
+++ yuumi.py
@@ -1,72 +1,75 @@
 from contextlib import nullcontext
 from types import WrapperDescriptorType
 
-
 class Yuumi:
 
-    def __init__(itemOne, itemTwo, itemThree, itemFour, itemFive, itemSix, ward):
+    def __init__(self):
+        self.itemOne = self.itemTwo = self.itemThree = self.itemFour = self.itemFive = self.itemSix = self.ward = None
+        pass
+
+    #Setters -- These will have to be incredibly complex actually.......
     
-        itemOne, itemTwo, itemThree, itemFour, itemFive, itemSix, ward = None, None, None, None, None, None, None
+    def setItemOne(self, itemOne):
+        self.itemOne = itemOne
 
-        #Setters -- These will have to be incredibly complex actually.......
-        
-        def setItemOne(self, itemOne):
-            self.itemOne = itemOne
+    def setItemTwo(self, itemTwo):
+        self.itemTwo = itemTwo
 
-        def setItemTwo(self, itemTwo):
-            self.itemTwo = itemTwo
+    def setItemThree(self, itemThree):
+        self.itemThree = itemThree
 
-        def setItemThree(self, itemThree):
-            self.itemThree = itemThree
+    def setItemFour(self, itemFour):
+        self.itemFour = itemFour
 
-        def setItemFour(self, itemFour):
-            self.itemFour = itemFour
+    def setItemFive(self, itemFive):
+        self.itemFive = itemFive
 
-        def setItemFive(self, itemFive):
-            self.itemFive = itemFive
+    def setItemSix(self, itemSix):
+        self.itemSix = itemSix
 
-        def setItemSix(self, itemSix):
-            self.itemSix = itemSix
+    def setWard(self, ward):
+        self.ward = ward
 
-        def setWard(self, ward):
-            self.ward = ward
+    #Getters
 
-        #Getters
+    def getItemOne(self):
+        return self.itemOne
 
-        def getItemOne(self):
-            return itemOne
+    def getItemTwo(self):
+        return self.itemTwo
+    
+    def getItemThree(self):
+        return self.itemThree
+    
+    def getItemFour(self):
+        return self.itemFour
+    
+    def getItemFive(self):
+        return self.itemFive
+    
+    def getItemSix(self):
+        return self.itemSix
+    
+    def getWard(self):
+        return self.ward
 
-        def getItemTwo(self):
-            return itemTwo
-        
-        def getItemThree(self):
-            return itemThree
-        
-        def getItemFour(self):
-            return itemFour
-        
-        def getItemFive(self):
-            return itemFive
-        
-        def getItemSix(self):
-            return itemSix
-        
-        def getWard(self):
-            return ward
+    #Status
 
-        #Status
+    def hasQ():
+        return False
 
-        def hasQ():
-            return False
+    def hasW():
+        return False
 
-        def hasW():
-            return False
+    def hasE():
+        return False
 
-        def hasE():
-            return False
+    def hasR():
+        return False
 
-        def hasR():
-            return False
+    #Shop
+    
+    #Heal
 
         
         
