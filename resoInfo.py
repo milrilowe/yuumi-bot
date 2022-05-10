1667fcaab509c836845fa7d45464c94d6edb32f2
Miles Owens <milesrhowens@gmail.com>
2022-05-10 15:01:19 -0700

    nothing

--- resoInfo.py
+++ resoInfo.py
@@ -51,7 +51,7 @@ else:
 try:
     while True:
         x, y = auto.position()
-        rgb = ahk.pixel_get_color(x, 975)
+        rgb = ahk.pixel_get_color(x, y)
         rgb = rgb[2:]
         r, g, b = tuple(int(rgb[i:i+2], 16) for i in (0, 2, 4))
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4) + ' R: ' + str(r).rjust(4) + ' G: ' + str(g).rjust(4) + ' B: ' +str(b).rjust(4)
