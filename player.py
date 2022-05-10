ed7a3c4ed363d6dc45c7bc558f742d8cb92c6c4a
Miles Owens <milesrhowens@gmail.com>
2022-05-10 05:41:17 -0700

    we are not ready for player, yet - player.py will be like, game logic/decision-making

--- player.py
+++ player.py
@@ -1,18 +1,3 @@
-import pyautogui as auto
-import keyboard, time
+import yuumi
 
-time.sleep(2)
-
-while True:
-    auto.moveTo(1570, 600)
-    keyboard.press_and_release('w')
-    auto.moveTo(1680, 600)
-    keyboard.press_and_release('w')
-    auto.moveTo(1790, 600)
-    keyboard.press_and_release('w')
-    auto.moveTo(1900, 600)
-    keyboard.press_and_release('w')
-    if keyboard.is_pressed('q'):
-        print('you quit')
-        break
-
+#This shouldn't have any coordinate or pixel values.  It should pretty much be all in English e.g. if yuumi.hasW : yuumi.attach etc.
